7ab79a77ec01353bb3a524d265e5a77392550f8e
Michael Bridgen <michael@weave.works>
2018-03-06 16:16:03 +0000

    Use kubeyaml for updating policy
    kubeyaml (now) supports updating annotations as well as images, with
    the same round-tripping properties.
    
    One wrinkle in the test code is that it relies on maps being sorted
    lexicographically when interpolated into templates. However, kubeyaml
    keeps the order in which things were added. Rather than add a lot of
    complication in the comparison of expected v. actual result, I have
    made the order explicit by using slices instead of maps.
    
    We can get rid of some duplication by using the resources package to
    parse manifests and thereby get policies.

--- cluster/kubernetes/policies.go
+++ cluster/kubernetes/policies.go
@@ -1,9 +1,9 @@
 package kubernetes
 
 import (
-	"io/ioutil"
-	"regexp"
-	"strings"
+	"bytes"
+	"fmt"
+	"os/exec"
 
 	"github.com/pkg/errors"
 	yaml "gopkg.in/yaml.v2"
@@ -13,111 +13,72 @@ import (
 	"github.com/weaveworks/flux/policy"
 )
 
-func (m *Manifests) UpdatePolicies(in []byte, update policy.Update) ([]byte, error) {
-	tagAll, _ := update.Add.Get(policy.TagAll)
-	return updateAnnotations(in, tagAll, func(a map[string]string) map[string]string {
-		for p, v := range update.Add {
-			if p == policy.TagAll {
-				continue
-			}
-			a[resource.PolicyPrefix+string(p)] = v
-		}
-		for p, _ := range update.Remove {
-			delete(a, resource.PolicyPrefix+string(p))
-		}
-		return a
-	})
-}
+func (m *Manifests) UpdatePolicies(in []byte, resourceID flux.ResourceID, update policy.Update) ([]byte, error) {
+	ns, kind, name := resourceID.Components()
 
-func updateAnnotations(def []byte, tagAll string, f func(map[string]string) map[string]string) ([]byte, error) {
-	manifest, err := parseManifest(def)
-	if err != nil {
-		return nil, err
-	}
-	annotations := manifest.Metadata.AnnotationsOrNil()
-	if tagAll != "" {
-		containers := manifest.Spec.Template.Spec.Containers
-		for _, c := range containers {
-			p := resource.PolicyPrefix + string(policy.TagPrefix(c.Name))
-			if tagAll != "glob:*" {
-				annotations[p] = tagAll
-			} else {
-				delete(annotations, p)
-			}
-		}
-	}
-	newAnnotations := f(annotations)
+	add := update.Add
+	del := update.Remove
 
-	// Write the new annotations back into the manifest
-	// Generate a fragment of the new annotations.
-	var fragment string
-	if len(newAnnotations) > 0 {
-		fragmentB, err := yaml.Marshal(map[string]map[string]string{
-			"annotations": newAnnotations,
-		})
+	// We may be sent the pseudo-policy `policy.TagAll`, which means
+	// apply this filter to all containers. To do so, we need to know
+	// what all the containers are.
+	if tagAll, ok := add.Get(policy.TagAll); ok {
+		add = add.Without(policy.TagAll)
+		containers, err := parseForContainers(in)
 		if err != nil {
 			return nil, err
 		}
-
-		fragment = string(fragmentB)
-
-		// Remove the last newline, so it fits in better
-		fragment = strings.TrimSuffix(fragment, "\n")
-
-		// indent the fragment 2 spaces
-		// TODO: delete all regular expressions which are used to modify YAML.
-		// See #1019. Modifying this is not recommended.
-		fragment = regexp.MustCompile(`(.+)`).ReplaceAllString(fragment, "  $1")
-
-		// Add a newline if it's not blank
-		if len(fragment) > 0 {
-			fragment = "\n" + fragment
-		}
-	}
-
-	// Find where to insert the fragment.
-	// TODO: delete all regular expressions which are used to modify YAML.
-	// See #1019. Modifying this is not recommended.
-	replaced := false
-	annotationsRE := regexp.MustCompile(`(?m:\n  annotations:\s*(?:#.*)*(?:\n    .*|\n)*$)`)
-	newDef := annotationsRE.ReplaceAllStringFunc(string(def), func(found string) string {
-		if !replaced {
-			replaced = true
-			return fragment
-		}
-		return found
-	})
-	if !replaced {
-		metadataRE := multilineRE(`(metadata:\s*(?:#.*)*)`)
-		newDef = metadataRE.ReplaceAllStringFunc(string(def), func(found string) string {
-			if !replaced {
-				replaced = true
-				f := found + fragment
-				return f
+		for _, c := range containers {
+			// Special case: glob:* is the same as "allow anything",
+			// i.e., don't have a filter.
+			if tagAll == "glob:*" {
+				del = del.Add(policy.TagPrefix(c))
+			} else {
+				add = add.Set(policy.TagPrefix(c), tagAll)
 			}
-			return found
-		})
-	}
-	if !replaced {
-		return nil, errors.New("Could not update resource annotations")
+		}
 	}
 
-	return []byte(newDef), err
+	args := []string{"annotate", "--namespace", ns, "--kind", kind, "--name", name}
+
+	for pol, val := range add {
+		args = append(args, fmt.Sprintf("%s%s=%s", resource.PolicyPrefix, pol, val))
+	}
+	for pol, _ := range del {
+		args = append(args, fmt.Sprintf("%s%s=", resource.PolicyPrefix, pol))
+	}
+
+	cmd := exec.Command("kubeyaml", args...)
+	out := &bytes.Buffer{}
+	errOut := &bytes.Buffer{}
+	cmd.Stdin = bytes.NewBuffer(in)
+	cmd.Stdout = out
+	cmd.Stderr = errOut
+	err := cmd.Run()
+	if err != nil {
+		return nil, errors.Wrap(err, errOut.String())
+	}
+	return out.Bytes(), nil
 }
 
-type Manifest struct {
-	Metadata Metadata `yaml:"metadata"`
-	Spec     struct {
+type containerManifest struct {
+	Spec struct {
 		Template struct {
 			Spec struct {
-				Containers []Container `yaml:"containers"`
+				Containers []struct {
+					Name  string `yaml:"name"`
+					Image string `yaml:"image"`
+				} `yaml:"containers"`
 			} `yaml:"spec"`
 		} `yaml:"template"`
 		JobTemplate struct {
 			Spec struct {
 				Template struct {
 					Spec struct {
-						Containers []Container `yaml:"containers"`
+						Containers []struct {
+							Name  string `yaml:"name"`
+							Image string `yaml:"image"`
+						} `yaml:"containers"`
 					} `yaml:"spec"`
 				} `yaml:"template"`
 			} `yaml:"spec"`
@@ -125,93 +86,31 @@ type Manifest struct {
 	} `yaml:"spec"`
 }
 
-func (m Metadata) AnnotationsOrNil() map[string]string {
-	if m.Annotations == nil {
-		return map[string]string{}
-	}
-	return m.Annotations
-}
-
-type Metadata struct {
-	Name        string            `yaml:"name"`
-	Annotations map[string]string `yaml:"annotations"`
-}
-
-type Container struct {
-	Name  string `yaml:"name"`
-	Image string `yaml:"image"`
-}
-
-func parseManifest(def []byte) (Manifest, error) {
-	var m Manifest
+func parseForContainers(def []byte) ([]string, error) {
+	var m containerManifest
 	if err := yaml.Unmarshal(def, &m); err != nil {
-		return m, errors.Wrap(err, "decoding annotations")
+		return nil, errors.Wrap(err, "decoding manifest")
 	}
-	return m, nil
+
+	var names []string
+	for _, c := range m.Spec.Template.Spec.Containers {
+		names = append(names, c.Name)
+	}
+	for _, c := range m.Spec.JobTemplate.Spec.Template.Spec.Containers {
+		names = append(names, c.Name)
+	}
+	return names, nil
 }
 
 func (m *Manifests) ServicesWithPolicies(root string) (policy.ResourceMap, error) {
-	all, err := m.FindDefinedServices(root)
+	resources, err := m.LoadManifests(root)
 	if err != nil {
 		return nil, err
 	}
 
-	result := map[flux.ResourceID]policy.Set{}
-	err = iterateManifests(all, func(s flux.ResourceID, m Manifest) error {
-		ps, err := policiesFrom(m)
-		if err != nil {
-			return err
-		}
-		result[s] = ps
-		return nil
-	})
-	if err != nil {
-		return nil, err
+	polMap := policy.ResourceMap{}
+	for _, res := range resources {
+		polMap[res.ResourceID()] = res.Policy()
 	}
-	return result, nil
-}
-
-func iterateManifests(services map[flux.ResourceID][]string, f func(flux.ResourceID, Manifest) error) error {
-	for serviceID, paths := range services {
-		if len(paths) != 1 {
-			continue
-		}
-
-		def, err := ioutil.ReadFile(paths[0])
-		if err != nil {
-			return err
-		}
-		manifest, err := parseManifest(def)
-		if err != nil {
-			return err
-		}
-
-		if err = f(serviceID, manifest); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func policiesFrom(m Manifest) (policy.Set, error) {
-	var policies policy.Set
-	for k, v := range m.Metadata.AnnotationsOrNil() {
-		if !strings.HasPrefix(k, resource.PolicyPrefix) {
-			continue
-		}
-		p := policy.Policy(strings.TrimPrefix(k, resource.PolicyPrefix))
-		if policy.Boolean(p) {
-			if v != "true" {
-				continue
-			}
-			policies = policies.Add(p)
-		} else {
-			policies = policies.Set(p, v)
-		}
-	}
-	return policies, nil
-}
-
-func multilineRE(lines ...string) *regexp.Regexp {
-	return regexp.MustCompile(`(?m:^` + strings.Join(lines, "\n") + `$)`)
+	return polMap, nil
 }
